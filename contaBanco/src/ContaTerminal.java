674c2c4d68fe7bbe909d81d85f7857ebe57906f1
Danielle Almeida <danicalmeida85@hotmail.com>
2026-07-14 11:04:48 -0300

    meu projeto conta banco

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+       //TODO: Conhecer e importar a classe Scanner
+
+       //Exibir as mensagens para o nosso usuário
+
+       //Obter pela scanner os valores digitados no terminal
+
+       //Exibir a mensagem conta criada
+
+       var scanner = new Scanner(System.in);
+
+       
+       System.out.println("Por favor, digite seu nome!");
+       String nome = scanner.nextLine();
+       
+       System.out.println("Por favor, digite o número da agência!");
+       String agencia = scanner.next();
+
+       System.out.println("Por favor, digite o numero da conta!");
+       int conta = scanner.nextInt();
+
+       System.out.println("Por favor, digite seu saldo!");
+       double saldo = scanner.nextDouble();
+
+       System.out.printf("Olá, %s, obrigado por criar uma conta em nosso banco, sua agencia é %s, conta %s e seu saldo %.2f já está disponível para saque.", nome, agencia, conta, saldo);
+
+       scanner.close();
     }
 }
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Usuario {
     public static void main(String[] args) throws Exception {
-       
+
         SmartTv smartTv = new SmartTv();
 
         smartTv.diminuirVolume();
